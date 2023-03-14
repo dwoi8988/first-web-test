153d474333137f7a8c623dcc6ba0f9084ebe4678
dwoi8988 <126851651+dwoi8988@users.noreply.github.com>
2023-03-14 16:27:35 +0900

    colors.js 추가, 모든 웹페이지에 다크 모드 추가!

---
+++ colors.js
@@ -0,0 +1,55 @@
+var Links = 
+{
+    setColor:function (color) 
+    {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length)
+    {
+    alist[i].style.color = color;
+    i = i + 1; 
+    }
+    }
+}
+
+
+var Body = 
+{
+    setColor:function (color)
+    {
+    document.querySelector('body').style.color = color;
+    }
+    ,
+    setBackgroundColor : function (color)
+    {
+    document.querySelector('body').style.backgroundColor = color;
+
+    }
+}
+
+
+function darkmodehandler(self)
+{
+    var target = document.querySelector('body');
+if 
+   (self.value == '다크on')
+    {
+        Body.setColor('white');
+        Body.setBackgroundColor('black');
+        self.value = '다크off';
+    
+    
+    Links.setColor('powderblue');
+    }
+    
+else 
+{
+        {
+        Body.setColor('black');
+        Body.setBackgroundColor('white');
+        self.value = '다크on';
+
+        Links.setColor('blue');
+        }
+}
+}
